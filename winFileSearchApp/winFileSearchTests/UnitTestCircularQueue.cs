9b72764636f126945359bd3300fa1821998b6171
Pavlov Andrey <pandvik@gmail.com>
2015-11-09 21:16:18 +0300

    Added search in file method

---
+++ winFileSearchApp/winFileSearchTests/UnitTestCircularQueue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using winFileSearchLib;
+
+namespace winFileSearchTests
+{
+    [TestClass]
+    public class UnitTestCircularQueue
+    {
+        [TestMethod]
+        public void CircularQueue_push_cmp()
+        {
+            CircularQueue<int> x = new CircularQueue<int>(3);
+            x.push(10);
+            Assert.IsTrue(x.cmp(new[] { 10 }));
+            x.push(15);
+            Assert.IsTrue(x.cmp(new[] { 10, 15}));
+            x.push(20);
+            Assert.IsTrue(x.cmp(new[] { 10, 15, 20 }));
+            x.push(25);
+            Assert.IsTrue(x.cmp(new[] { 15, 20, 25 }));
+        }
+    }
+}
